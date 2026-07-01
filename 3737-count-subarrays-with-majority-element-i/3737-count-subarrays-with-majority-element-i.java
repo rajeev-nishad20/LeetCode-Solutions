05a6adba69c0dfa6294fa89d0850d0f4d55818f0
rajeev-nishad20 <nrajeev610@gmail.com>
2026-07-01 14:23:44 +0530

    Time: 34 ms (69.61%), Space: 47.3 MB (6.37%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+
+            for (int j = i; j < n; j++) {
+                if (nums[j] == target) cnt++;
+
+                int len = j - i + 1;
+
+                if (2 * cnt > len)
+                    ans++;
+            }
+        }
+
+        return (int)ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
